c77bbf0b678535bb0cc2a983faaa237f35b6f264
zwer <414356480@qq.com>
2021-05-21 13:08:03 +0800

    feat: 采用 gpu 运算

--- pytorch_demo/05photo_classsfier/05use_gpu.py
+++ pytorch_demo/05photo_classsfier/05use_gpu.py
@@ -1,6 +1,8 @@
 import torch
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+use_gpu = torch.cuda.is_available()
+print(use_gpu)
 
 # Assume that we are on a CUDA machine, then this should print a CUDA device:
 print(device)
---
+++ pytorch_demo/05photo_classsfier/06tool_gpu.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+"""
+数据并行自动拆分了你的数据并且将任务单发送到多个 GPU 上。
+当每一个模型都完成自己的任务之后，DataParallel 收集并且合并这些结果，然后再返回给你。
+"""
+
+
+class RandomDataset(Dataset):
+    """
+        随机数数据集
+    """
+
+    def __init__(self, size, length):
+        self.len = length
+        self.data = torch.randn(length, size)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return self.len
+
+
+input_size = 5
+output_size = 2
+
+batch_size = 30
+data_size = 100
+
+# device use gpu or cpu
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+rand_loader = DataLoader(dataset=RandomDataset(input_size, data_size), batch_size=batch_size, shuffle=True)
+
+
+class Model(nn.Module):
+    # Our model
+    def __init__(self, input_size, output_size):
+        super(Model, self).__init__()
+        self.fc = nn.Linear(input_size, output_size)
+
+    def forward(self, input):
+        output = self.fc(input)
+        print("\tIn Model: input size", input.size(), "output size", output.size())
+        return output
+
+
+model = Model(input_size, output_size)
+if torch.cuda.device_count() > 1:
+    print("Let's use", torch.cuda.device_count(), "GPUs!")
+    # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
+    model = nn.DataParallel(model)
+
+model.to(device)
+
+for data in rand_loader:
+    input = data.to(device)
+    output = model(input)
+    print("Outside: input size", input.size(), "output_size", output.size())
