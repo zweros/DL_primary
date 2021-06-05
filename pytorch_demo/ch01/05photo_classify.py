5591bda8f237b599ea465513f3b522117db31efa
zwer <414356480@qq.com>
2021-06-05 19:36:25 +0800

    bug: 解决模型在 gpu 出错的问题，并增大训练的 epoch

--- pytorch_demo/ch01/05photo_classify.py
+++ pytorch_demo/ch01/05photo_classify.py
@@ -61,12 +61,12 @@ if __name__ == '__main__':
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=False, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                              shuffle=True, num_workers=2)
+                                              shuffle=True, num_workers=1)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                            download=False, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                             shuffle=False, num_workers=2)
+                                             shuffle=False, num_workers=1)
 
     classes = ('plane', 'car', 'bird', 'cat',
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
@@ -86,9 +86,9 @@ if __name__ == '__main__':
     import torch.optim as optim
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-    for epoch in range(2):  # loop over the dataset multiple times
+    # optimizer = optim.(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    for epoch in range(10):  # loop over the dataset multiple times
 
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
@@ -133,6 +133,6 @@ if __name__ == '__main__':
             outputs = net(images.to(device))
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+            correct += (predicted == labels.to(device)).sum().item()
 
     print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
